f2e172f88bc80353aa8853e2090faa1aa6f1d03d
Marek Machliński <marekin450@spoko.pl>
2020-04-23 13:02:22 +0200

    Added database converter classes.

---
+++ kalkulatorcl/app/converters/MarkToMarkJson.java
@@ -0,0 +1,22 @@
+package converters;
+
+import json.MarkJson;
+import models.Mark;
+ 
+import javax.inject.Singleton;
+import java.util.function.Function;
+
+@Singleton
+public class MarkToMarkJson implements Function<Mark, MarkJson> {
+    @Override
+    public MarkJson apply(Mark mark) {
+
+        return new MarkJson(
+                mark.id,
+                mark.weight, 
+                mark.user.id,
+                mark.score.id,
+                mark.subject.id);
+    }
+
+}

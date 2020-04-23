ce776bc75db377c07aaa786b6e854d156d908a53
Radosław Stanulewicz <rad8broda@gmail.com>
2020-04-23 13:30:37 +0200

    Add json mapping for models

---
+++ kalkulatorcl/app/json/ScoreJson.java
@@ -0,0 +1,15 @@
+package json;
+
+public class ScoreJson {
+
+    public long id;
+    public float value;
+
+    public ScoreJson(long id, float value) {
+        this.id = id;
+        this.value = value;
+    }
+ 
+    public ScoreJson() {
+    }
+}
---
+++ kalkulatorcl/app/json/SubjectJson.java
@@ -0,0 +1,16 @@
+package json;
+
+public class SubjectJson {
+
+    public long id;
+    public String name;
+
+    public SubjectJson(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+ 
+    public SubjectJson() {
+    }
+
+}

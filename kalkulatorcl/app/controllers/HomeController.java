d2d41fd4b489475be2c7c6ccf2d463f4465cdec6
Radosław Stanulewicz <rad8broda@gmail.com>
2020-05-22 22:11:38 +0200

    Add parsing values to list

--- kalkulatorcl/app/controllers/HomeController.java
+++ kalkulatorcl/app/controllers/HomeController.java
@@ -9,6 +9,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -22,7 +24,9 @@ public class HomeController extends Controller {
     public HomeController(final FormFactory formFactory) {
         this.formFactory = formFactory;
     }
-    
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
     
     /**
      * An action that renders an HTML page with a welcome message.
@@ -47,6 +51,10 @@ public class HomeController extends Controller {
         String script = dynamicForm.get("script");
         Document doc = Jsoup.parse(script);
         String output = "";
+        List<String> id = new ArrayList<String>();
+        List<String> name = new ArrayList<String>();
+        List<Integer> weight = new ArrayList<Integer>();
+        List<Float> value = new ArrayList<Float>();
         for (Element table : doc.select("table.KOLOROWA")) {
         for (Element row : table.select("tr")) {
             Elements tds = row.select("td tr");
@@ -58,14 +66,30 @@ public class HomeController extends Controller {
                     i ++;
                     continue;
                 }
-                output += element.text();
-                output += "\n";
+                // output += element.text();
+                // output += "\n";
+                output = element.text();
+                String[] array = output.split(" ");
+                id.add(array[0]);
+                name.add(array[1]);
+                if(isNumeric(array[array.length-2]))
+                {
+                    weight.add(Integer.parseInt(array[array.length-2]));
+                    value.add(Float.parseFloat(array[array.length-1]));
+                }
+                else
+                {
+                    weight.add(Integer.parseInt(array[array.length-1]));
+                    value.add(0.0f);
+                }
+                
+
+                i++;
+
             }
            }
         }
-        
-
-        return ok(output);
+        return ok(value.toString());
     }
 
 }
